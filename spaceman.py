a1db75587cdea27d4c551a15c2bd4f19dd664e08
Kye Williams <kwilliams154517@troy.edu>
2019-09-11 16:28:43 -0700

    Final project Finally

--- spaceman.py
+++ spaceman.py
@@ -1,13 +1,14 @@
-import random
+from random import shuffle
 
 answerlist = ["fruit", "candy", "pizza", "butter", "cake", "pastry", "icecream","ramen"]
 
-random.shuffle(answerlist)
+shuffle(answerlist)
 
 answer = list(answerlist[0])
 
 display = []
 used = []
+# .extend allows len of list to be updated no matter how many times the list itself is updated with new words or new word being taken away 
 display.extend(answer)
 used.extend(display)
 
@@ -16,6 +17,7 @@ used.extend(display)
 for i in range(len(display)):
     display[i] = "_"
 print(' '.join(display))
+print()
 print(str(len(answer)) + " = number of letters in each word")
 print()
 
